2e5bbe6f76cf5e7e1edd9bfdaa7b3fe02fafb53f
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2024-02-03 19:46:21 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 
@@ -13,7 +15,24 @@ mongoose.connect(
 })
 
 const app=express();
-app.listen(300,()=>{
+app.use(express.json());//by deafult we are not allow to send json to server so here we allow to send json by this line of code
+
+
+
+app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
 
+
+// app.get('/test',(req,res)=>{
+//     res.send('hello world')
+// });
+
+// app.get("/test", (req, res) => {
+//   res.json({
+//     message:"hello world!",
+//   })
+// });
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
